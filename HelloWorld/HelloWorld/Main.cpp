80fb20eda58046f413b427f9b637ea7c8b001221
Alex Stanescu <alexanderstanescu91@gmail.com>
2018-05-05 15:58:18 -0700

    added Window init code

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#include <Windows.h>
 
-int main(int argc, char **argv) {
-	std::cout << "Hello World!" << std::endl;
+static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrvInstance, LPSTR lpCmdLine, int nCmdShow) {
+	WNDCLASS wndClass = {};
+	wndClass.lpfnWndProc = WndProc;
+	wndClass.hInstance = hInstance;
+	wndClass.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW;
+	wndClass.lpszClassName = "Window";
+
+	RegisterClass(&wndClass);
+
+	HWND hWnd = CreateWindow("Window", "Hello World", WS_OVERLAPPED | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, hInstance, 0);
+
+	MSG msg = {};
+	while (GetMessage(&msg, 0, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
 	return 0;
+}
+
+static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	return DefWindowProc(hWnd, message, wParam, lParam);
 }
